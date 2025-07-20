264e32090cfcf6c0fdb1d53bc0cdfd34ba3bb203
yogesh jadhav <yogeshinfo1245@gmail.com>
2025-07-20 19:14:26 +0530

    url changr

--- frontend/src/components/admin/PostJob.jsx
+++ frontend/src/components/admin/PostJob.jsx
@@ -42,7 +42,7 @@ const PostJob = () => {
         e.preventDefault();
         try {
             setLoading(true);
-            const res = await axios.post(`${JOB_API_END_POINT}/post`, input,{
+            const res = await axios.post(`https://job-portal-oee3.onrender.com/api/v1/post`, input,{
                 headers:{
                     'Content-Type':'application/json'
                 },
